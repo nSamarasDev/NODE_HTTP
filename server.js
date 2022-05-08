d92fa2294e3d38d6a2eaba4678ed31f816818989
Nicholas Samaras <n.samaras4@outlook.com>
2022-05-08 18:55:13 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
+const data = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+  { id: 3, text: 'Todo Three' },
+];
+
 const server = http.createServer((req, res) => {
+  res.setHeader('Content-type', 'text/plain');
+  res.setHeader('X-Powered-By', 'Node.js');
+  res.write('Hello');
   const { headers, url, method } = req;
   res.end();
+  JSON.stringify({
+    success: true,
+    data: todos,
+  });
 });
 
 const PORT = 5000;
